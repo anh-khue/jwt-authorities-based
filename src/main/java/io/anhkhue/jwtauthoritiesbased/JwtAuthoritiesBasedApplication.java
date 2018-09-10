72465749f21aed1bf5e948b5c0cbeefbafa54cb6
anh-khue <thailyanhkhue@gmail.com>
2018-09-10 22:33:45 +0700

    Create Spring Boot project.

---
+++ src/main/java/io/anhkhue/jwtauthoritiesbased/JwtAuthoritiesBasedApplication.java
@@ -0,0 +1,12 @@
+package io.anhkhue.jwtauthoritiesbased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthoritiesBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthoritiesBasedApplication.class, args);
+	}
+}
